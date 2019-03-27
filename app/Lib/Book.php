bcafd6157ab9ed37dfdf8c5bf99b476655f3f52c
AKHILESH KUMAR <akhileshraw1@gmail.com>
2019-03-27 13:59:42 +0530

    index function was written

--- app/Lib/Book.php
+++ app/Lib/Book.php
@@ -1,6 +1,19 @@
 <?php
-namespace App\Lib;
-use App\Models\Book;
 
+namespace App\Lib;
+
+use App\Models\Book as Model;
+
+class Book
+{
+
+  public static function index()
+  {
+      $books = Model::all();
+      return $books;
+  }
+
+
+}
 
  ?>
