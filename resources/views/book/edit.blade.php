cf68042d30d177c20bd77d581485158678fd2fa6
AKHILESH KUMAR <akhileshraw1@gmail.com>
2019-03-28 19:05:57 +0530

    update in edit page as description was not appearing on page

--- resources/views/book/edit.blade.php
+++ resources/views/book/edit.blade.php
@@ -8,23 +8,23 @@
        @csrf
          <div class="form-group">
              <label for="title">Book Title:</label>
-             <input type="text" class="form-control" name="title" value=" {{$book['title']}}" required >
+             <input type="text" class="form-control" name="title" value=" {{$book['title']}}"  >
          </div>
          <div class="form-group">
              <label for="author">Book Author:</label>
-             <input type="text" class="form-control" name="author" value="{{$book['author']}}" required>
+             <input type="text" class="form-control" name="author" value="{{$book['author']}}" >
          </div>
          <div class="form-group">
          <label for="description">Book Description:</label>
-         <textarea cols="5" rows="5" class="form-control" name="description" value="{{$book['description']}}" required></textarea>
+         <textarea cols="5" rows="5" class="form-control" name="description"> {{$book['description']}}</textarea>
          </div>
          <div class="form-group">
              <label for="quantity">Enter Quantity:</label>
-             <input type="number" class="form-control" name="quantity" value="{{$book['quantity']}}" required>
+             <input type="number" class="form-control" name="quantity" value="{{$book['quantity']}}" >
          </div>
          <div class="form-group">
              <label for="price">Book Price:</label>
-             <input type="number" step="any" class="form-control" name="price" value="{{$book['price']}}" required>
+             <input type="number" step="any" class="form-control" name="price" value="{{$book['price']}}" >
          </div>
          <div class="form-group">
          <label for="image">upload Image:</label>
