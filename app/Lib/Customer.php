c5be7a8374369aff04e2f93482f3165ad9ec7c7e
AKHILESH KUMAR <akhileshraw1@gmail.com>
2019-03-29 16:22:47 +0530

    Customer Class created in Lib

---
+++ app/Lib/Customer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Lib;
+
+use App\Models\Book as Model;
+
+class Customer
+{
+
+  public static function index()
+  {
+      
+  }
+
+  public static function store($request)
+  {
+
+
+  }
+
+  public static function show($id)
+  {
+
+  }
+
+  public static function edit($id)
+  {
+
+  }
+
+  public static function update($request, $id)
+  {
+
+  }
+
+  public static function destroy($id)
+  {
+
+  }
+
+
+}
+
+ ?>
